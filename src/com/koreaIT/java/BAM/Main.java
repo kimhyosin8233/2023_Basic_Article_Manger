c972a912eb22e7bad94479a5eb12005dd3b86248
kimhyosin8233 <hyosin8233@gmail.com>
2023-01-15 01:48:31 +0900

    게시물 리스트 기능 구현중

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -1,5 +1,7 @@
 package com.koreaIT.java.BAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,18 +13,22 @@ public class Main {
 		
 		int lastArticleId = 0;
 		
+		List<Article> article = new ArrayList<>();
+		
 		while(true) {
 			
 			System.out.printf("명령어) ");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
 			
 			if(cmd.length() == 0) {
 				System.out.println("명령어를 입력해주세요");
 				continue;
 			}
+			
 			if(cmd.equals("exit")) {
 				break;
 			}
+			
 			if(cmd.equals("article write")) {
 				int id = lastArticleId + 1;
 				lastArticleId = id;
@@ -30,10 +36,10 @@ public class Main {
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
+				
+				Article article = new Article(id, title, body); 
 	
 				System.out.printf("%d번글이 생성되었습니다\n", id);
-				
-//				System.out.printf("%s, %s\n", title, body);
 				
 			}else if (cmd.equals("article list")) {
 				System.out.println("게시물이 없습니다");
@@ -46,6 +52,21 @@ public class Main {
 		
 		sc.close();
 	}
+	
 }
+
+class Article{
+	int id;
+	String title;
+	String body;
+	
+	Article(int id, String title, String body){
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+}
+
+
 
 
